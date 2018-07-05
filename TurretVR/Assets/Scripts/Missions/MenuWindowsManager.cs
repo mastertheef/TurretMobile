3514b07da5c77c71b1ba7c8bdddc6ebeafdf0c4c
Yaroslav Oliinyk <niochronos@gmail.com>
2018-07-05 23:01:12 +0300

    add InventorySelectWindow, ShopSelectWindow, MenuWindowsManager

---
+++ TurretVR/Assets/Scripts/Missions/MenuWindowsManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuWindowsManager : MonoBehaviour {
+
+	#region Fields
+
+	[Header("Buttons")]
+	public Button levelSelectToInventoryButton;
+	public Button inventoryToShopButton;
+	public Button shopToInventorySelectButton;
+	public Button inventoryToLevelSelectButton;
+
+	[Header("Windows")]
+	public GameObject levelSelectWindow;
+	public GameObject inventorySelectWindow;
+	public GameObject shopSelectWindow;
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clickOnLevelSelectToInventoryButton()
+	{
+		levelSelectWindow.SetActive(false);
+		inventorySelectWindow.SetActive(true);
+	}
+
+	public void clickOnInventoryToShopButton()
+	{
+		inventorySelectWindow.SetActive(false);
+		shopSelectWindow.SetActive(true);
+	}
+
+	public void clickOnShopToInventorySelectButton()
+	{
+		shopSelectWindow.SetActive(false);
+		inventorySelectWindow.SetActive(true);
+	}
+
+	public void clickOnInventoryToLevelSelectButton()
+	{
+		inventorySelectWindow.SetActive(false);
+		levelSelectWindow.SetActive(true);
+	}
+}
